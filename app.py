37c7e592fcac31575134fcdbd73a315647af0f04
otabek <otabekbarotov15@gmail.com>
2026-03-11 19:49:36 +0500

    styles added

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 
+students_data = {
+    "Abdul-Aziz": 77, "Abdullo": 47, "Abubakr": 59,
+    "Ahmadjon": 69, "Amir": 44, "Behruz": 69,
+    "Habibulloh": 51, "Ilyos": 64, "Maftunbek": 39,
+    "Maryam": 28, "Mubarro": 22, "Najmiya": 60,
+    "Otabek": 48, "Shahrom": 83, "Habibullo": 63,
+    "Rukhshona": 31, "Osiya": 69, "Bakhtiyor": 32,
+    "Muhammadjon": 0, "Timur": 89, "Gulsum": 36,
+    "Rayhona": 39, "Abduvozit": 12, "Ismoiljon": 0,
+    "Nizar": 72,
+}
+
+
 @app.route("/")
 def main():
     return render_template("/main_page.html")
@@ -11,9 +24,9 @@ def main():
 def about():
     return render_template("/about.html")
 
-@app.route("/student/<name>")
-def student(name):
-    return f"Hello {name}"
+@app.route("/students")
+def student():
+    return render_template("/students.html")
 
 
 @app.route("/grade/<name>/<int:score>")
