2fe306887c9861a60bb07cb4ef44ac6731c4dbee
otabek <otabekbarotov15@gmail.com>
2026-03-11 19:59:09 +0500

    /students function was added

--- app.py
+++ app.py
@@ -24,10 +24,24 @@ def main():
 def about():
     return render_template("/about.html")
 
-@app.route("/students")
-def student():
-    return render_template("/students.html")
 
+
+@app.route('/students')
+def students():
+ students_data = {
+    "Abdul-Aziz": 77, "Abdullo": 47, "Abubakr": 59,
+    "Ahmadjon": 69, "Amir": 44, "Behruz": 69,
+    "Habibulloh": 51, "Ilyos": 64, "Maftunbek": 39,
+    "Maryam": 28, "Mubarro": 22, "Najmiya": 60,
+    "Otabek": 48, "Shahrom": 83, "Habibullo": 63,
+    "Rukhshona": 31, "Osiya": 69, "Bakhtiyor": 32,
+    "Muhammadjon": 0, "Timur": 89, "Gulsum": 36,
+    "Rayhona": 39, "Abduvozit": 12, "Ismoiljon": 0,
+    "Nizar": 72,
+}
+ sorted_students = sorted(students_data.items(), key=lambda x: x[1], reverse=True)
+
+ return render_template('students.html', students=sorted_students)
 
 @app.route("/grade/<name>/<int:score>")
 def grade(name,score):
